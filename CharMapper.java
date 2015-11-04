02300d9c153e952197416836ba30ac8c3009aec2
PeterLudwig <peterdanludwig@gmail.com>
2015-11-04 12:05:06 -0500

    Update CharMapper.java

--- CharMapper.java
+++ CharMapper.java
@@ -5,12 +5,24 @@ import java.util.Scanner;
 public class CharMapper {
 	String filename;
 	String[][] charMap = new String[6][26];
+	public int spacing;
 
 	public CharMapper(String fontType) {	
 		filename = fontType;
+		switch (fontType) {
+			case "Bubble Letters":
+				spacing = 6; break;
+			case "Graffiti":
+				spacing = 8; break;
+			default:
+				spacing = 1; break;
+		}
 	}
-
+	public int getSpacing() {
+		return spacing;
+	}
 	public String[][] map() {
+		//dynamic file paths!!
 		String workingDirectory = System.getProperty("user.dir");
 		String absoluteFilePath = workingDirectory + File.separator + "src" + File.separator + filename + ".txt";
 		File file = new File(absoluteFilePath);
@@ -18,10 +30,10 @@ public class CharMapper {
 			int character = 0;
 			int line = 0;
 			Scanner scan = new Scanner(file);
-			while (line < 26 * 6) {
+			while (line < 26 * spacing) {
 				charMap[line%6][character]  = scan.nextLine();
 				++line;
-				if (line%6 == 0) {
+				if (line%spacing == 0) {
 					character++;
 				}
 			}
